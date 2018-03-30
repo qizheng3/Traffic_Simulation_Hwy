0c3761e7c44bc30cae4b24d1668860c574c5d267
pieworkplace <pieworkplace@gmail.com>
2018-03-30 00:49:20 -0400

    Update main.py
    Added UI into main

--- main.py
+++ main.py
@@ -1,26 +1,10 @@
-import vehicle
-import MultiLane
-import Lane
+    def mainloop(self):
+        self.root.mainloop()
+        
 
-def main():
-
-	highwayLength = 50000
-	vMax = 40
-	nLane = 5
-	highway = MultiLane.MultiLane(highwayLength, nLane, vMax)
-
-	iteration = 200
-
-	for i in range(iteration):
-
-		#highway.printSpeed();
-		highway.updateSpeed()
-		highway.updatePosition()
-		highway.checkChangeLaneLeft()
-		highway.checkChangeLaneRight()
-		highway.enterAtStart(0.5)
-		highway.exitAtEnd()
-		highway.entranceEvent(0.3, 0.4)
 
 if __name__ == '__main__':
-	main()
+    UI = UI()
+    for highway in range(10):
+        UI.display(highway)
+    UI.mainloop()
