133080030abd39635d17e25e50a34278751a70a4
pieworkplace <pieworkplace@gmail.com>
2018-03-30 00:49:59 -0400

    Update UI.py
    Changed some structure details

--- UI.py
+++ UI.py
@@ -25,20 +25,9 @@ class UI(object):
         self.figure = Figure(figsize = (self.animationSize[0], self.animationSize[1]), dpi = self.animationDpi)# set figure
         self.canvas = FigureCanvasTkAgg(self.figure, master = self.root) # TODO: subject to change root to frame
         self.canvas.get_tk_widget().grid(row = 0, column = 0, rowspan = 1) # TODO: subject to change canvas position
-    
-    def processData(self,i):
-        self.t = arange(0.0, 3, 0.01)
-        self.s = sin(2 * pi * self.t + i)
-
-    def drawFrame(self,i):
-        self.processData(i)
-        self.figure.clf()
-        self.figure.add_subplot(111).plot(self.t, self.s)
-        self.canvas.show()
-    
-    def display(self):
+        
         # set window in the center of the screen
-        # =====http://www.jb51.net/article/61962.htm=====
+        # ===== quote http://www.jb51.net/article/61962.htm =====
         self.root.update() # update window (must do)
         curWidth = self.root.winfo_reqwidth() # get current width
         curHeight = self.root.winfo_height() # get current height
@@ -46,13 +35,26 @@ class UI(object):
         # now generate configuration information
         tmpcnf = '%dx%d+%d+%d' % (curWidth, curHeight, (scnWidth - curWidth) / 2, (scnHeight - curHeight) / 2)
         self.root.geometry(tmpcnf)
-        #==========
-        
-        # run animation
-        for i in range(10):
-            self.drawFrame(i)
-        
-        # main loop
-        self.root.mainloop()
+        # ===== end quote =====
+    
+    def processData(self, highway):
+        return arange(0.0, 3, 0.1), sin(2 * pi * arange(0.0, 3, 0.1) + highway)
 
-UI().display()
+    def drawFrame(self, x, y):
+        self.figure.clf()
+        self.figure.add_subplot(111).scatter(x, y, s=3, color='r')
+        self.canvas.show()
+    
+    def display(self, highway):
+        x, y = self.processData(highway)
+        self.drawFrame(x, y)
+        
+    def mainloop(self):
+        self.root.mainloop()
+        
+
+if __name__ == '__main__':
+    UI = UI()
+    for highway in range(10):
+        UI.display(highway)
+    UI.mainloop()
