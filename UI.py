9482932b6d492999baa8fdc47e8a2fed5b9f1e81
pieworkplace <pieworkplace@gmail.com>
2018-03-30 17:49:16 -0400

    Update UI.py
    Added multithread.

--- UI.py
+++ UI.py
@@ -2,6 +2,8 @@ from Tkinter import *
 import matplotlib as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.figure import Figure
+import Queue
+import threading
 import warnings
 
 from numpy import arange, sin, pi
@@ -36,18 +38,32 @@ class UI(object):
         tmpcnf = '%dx%d+%d+%d' % (curWidth, curHeight, (scnWidth - curWidth) / 2, (scnHeight - curHeight) / 2)
         self.root.geometry(tmpcnf)
         # ===== end quote =====
+        
+        # set message queue
+        self.messageQueue = Queue.Queue()
+        self.processMessage()
     
     def processData(self, highway):
         return arange(0.0, 3, 0.1), sin(2 * pi * arange(0.0, 3, 0.1) + highway)
 
-    def drawFrame(self, x, y):
-        self.figure.clf()
-        self.figure.add_subplot(111).scatter(x, y, s=3, color='r')
-        self.canvas.show()
+    def processMessage(self):
+        self.root.after(500, self.processMessage) # check message queue every 0.5s
+        while not self.messageQueue.empty():
+            x, y = self.messageQueue.get()
+            self.figure.clf()
+            self.figure.add_subplot(111).scatter(x, y, s = 3, color = 'r')
+            self.canvas.show()
+            break
+            
     
-    def display(self, highway):
+    def drawFrame(self, highway):
         x, y = self.processData(highway)
-        self.drawFrame(x, y)
+        self.messageQueue.put((x, y))
+      
+    def display(self, highway):
+        pass
+
+        
         
     def mainloop(self):
         self.root.mainloop()
@@ -56,5 +72,5 @@ class UI(object):
 if __name__ == '__main__':
     UI = UI()
     for highway in range(10):
-        UI.display(highway)
+        pass
     UI.mainloop()
