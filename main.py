99bf5c7f937932e94b77a01f5e85946387dfc3f0
pieworkplace <pieworkplace@gmail.com>
2018-03-30 17:49:52 -0400

    Update main.py
    Added multithread in main for UI

--- main.py
+++ main.py
@@ -1,10 +1,45 @@
-    def mainloop(self):
-        self.root.mainloop()
-        
+import vehicle
+import MultiLane
+import Lane
+import UI
+import threading
+
+def main():
+
+	GUI = UI.UI()
+
+	def run():
+
+		highwayLength = 50000
+		vMax = 40
+		nLane = 5
+		highway = MultiLane.MultiLane(highwayLength, nLane, vMax)
+
+		iteration = 200
+
+		# print len(highway.lanes)
+		# print len(highway.lanes[0].cells)
+
+		for i in range(iteration):
+			#highway.printSpeed();
+			highway.updateSpeed()
+			highway.updatePosition()
+			highway.checkChangeLaneLeft()
+			highway.checkChangeLaneRight()
+			highway.enterAtStart(0.5)
+			highway.exitAtEnd()
+			highway.entranceEvent(0.3, 0.4)
+
+			#send msg to UI
+			GUI.drawFrame(i)
+
+	workerThread = threading.Thread(target = run)
+	workerThread.setDaemon(True)
+	workerThread.start()
+
+	GUI.mainloop()
+
 
 
 if __name__ == '__main__':
-    UI = UI()
-    for highway in range(10):
-        UI.display(highway)
-    UI.mainloop()
+	main()
