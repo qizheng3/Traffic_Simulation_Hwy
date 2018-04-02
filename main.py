a082af96fd1456900f79e5dee72b06e454aa3ba0
Haoting Li <lmhlht1994@qq.com>
2018-04-02 16:01:07 -0400

    mod

--- main.py
+++ main.py
@@ -13,12 +13,9 @@ def main():
         vMax = 40
         nLane = 3
         exitPt = 0.3
-<<<<<<< HEAD
         densityEntrance = 0.1
         probEnter = 0.3
         probExit = 0.4
-=======
->>>>>>> 05ef8ec4e791b21a8e0ec36a4e08439ed14116c3
         highway = MultiLane.MultiLane(highwayLength, 2 * nLane, exitPt, vMax)
         
         iteration = 400
@@ -29,15 +26,10 @@ def main():
             highway.updatePosition()
             highway.checkChangeLaneLeft()
             highway.checkChangeLaneRight()
-<<<<<<< HEAD
             highway.enterAtStart(densityEntrance)
             highway.exitAtEnd()
             highway.entranceEvent(probEnter, probExit)
-=======
-            highway.enterAtStart(0.1)
-            highway.exitAtEnd()
-            highway.entranceEvent(0.3, 0.4)
->>>>>>> 05ef8ec4e791b21a8e0ec36a4e08439ed14116c3
+
             
             # send data to UI
             GUI.display(highway)
