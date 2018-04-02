05ef8ec4e791b21a8e0ec36a4e08439ed14116c3
qizheng3 <zheng071511@gmail.com>
2018-04-02 09:35:45 -0400

    two-way hwy

--- Lane.py
+++ Lane.py
@@ -1,47 +1,47 @@
 import vehicle
 import random
 
+cellSize = 5
+
 # one single lane
 class Lane:
-	def __init__(self, l, vMax):
-		# density = total number of vehicles / total number of cells
-		self.density = 0.1  # initialize the vehicle density of one highway
-		self.vMax = vMax; # max speed for all vehicles in this lane
-		
-		self.size = l / 5;   # initialize the number of cells of one highway (unit length: 10 meters) ####updated: 5m
-		self.cells = [None] * self.size; # initialize the cell array
-		
-		#initialize some vehicles in the lane
-		for i in range(int(self.density * self.size)):
-			position = 0;
-			while True:
-				position = random.randint(0,self.size - 1);
-				if self.cells[position] == None:
-					break;
-			self.cells[position] = vehicle.Vehicle();
-
-		self.vNum = int(self.density * self.size); # number of vehicles in this lane
-		self.hasAccident = False; # one lane has no car accidents at the beginning
-
-	# add a new vehicle into a given position of one lane
-	def addCar(self, car, position):
-		if (self.cells[position] == None):
-			self.cells[position] = car;
-			self.vNum = self.vNum + 1;
-			return True;
-		else:
-			return False;
-
-	def RemoveCar(self, position):
-		if (self.cells[position] != None):
-			car = self.cells[position];
-			self.vNum = self.vNum - 1;
-			self.cells[position] = None;
-			return car;
-
-
-	#get some system status of this lane (e.g., density, occurence of accidents) 
-	def getPara(self):
-		self.density = self.vNum / self.size;
-		return self.density, self.hasAccident;
-
+    def __init__(self, l, vMax):
+        # density = total number of vehicles / total number of cells
+        self.density = 0.1  # initialize the vehicle density of one highway
+        self.vMax = vMax  # max speed for all vehicles in this lane
+        self.length = l	# length of each lane
+        self.size = l / cellSize  # initialize the number of cells of one highway (unit length: 10 meters) #### updated: 5m
+        self.cells = [None] * self.size  # initialize the cell array
+        
+        # initialize some vehicles in the lane
+        for i in range(int(self.density * self.size)):
+            position = 0;
+            while True:
+                position = random.randint(0, self.size - 1);
+                if self.cells[position] == None:
+                    break;
+            self.cells[position] = vehicle.Vehicle();
+        
+        self.vNum = int(self.density * self.size);  # number of vehicles in this lane
+        self.hasAccident = False;  # one lane has no car accidents at the beginning
+    
+    # add a new vehicle into a given position of one lane
+    def addCar(self, car, position):
+        if (self.cells[position] == None):
+            self.cells[position] = car;
+            self.vNum = self.vNum + 1;
+            return True;
+        else:
+            return False;
+    
+    def RemoveCar(self, position):
+        if (self.cells[position] != None):
+            car = self.cells[position];
+            self.vNum = self.vNum - 1;
+            self.cells[position] = None;
+            return car;
+    
+    # get some system status of this lane (e.g., density, occurence of accidents)
+    def getPara(self):
+        self.density = self.vNum / self.size;
+        return self.density, self.hasAccident;
--- main.py
+++ main.py
@@ -4,40 +4,38 @@ import Lane
 import UI
 import threading
 
+
 def main():
-
-	GUI = UI.UI()
-
-	def run():
-
-		highwayLength = 2500
-		vMax = 40
-		nLane = 5
-		highway = MultiLane.MultiLane(highwayLength, nLane, vMax)
-
-		iteration = 200
-
-		for i in range(iteration):
-			#highway.printSpeed();
-			highway.updateSpeed()
-			highway.updatePosition()
-			highway.checkChangeLaneLeft()
-			highway.checkChangeLaneRight()
-			highway.enterAtStart(0.1)
-			highway.exitAtEnd()
-			# highway.entranceEvent(0.3, 0.4)
-
-			#send data to UI
-			GUI.display(highway)
-
-
-	workerThread = threading.Thread(target = run)
-	workerThread.setDaemon(True)
-	workerThread.start()
-
-	GUI.mainloop()
-
+    GUI = UI.UI()
+    
+    def run():
+        highwayLength = 2500
+        vMax = 40
+        nLane = 3
+        exitPt = 0.3
+        highway = MultiLane.MultiLane(highwayLength, 2 * nLane, exitPt, vMax)
+        
+        iteration = 400
+        
+        for i in range(iteration):
+            # highway.printSpeed();
+            highway.updateSpeed()
+            highway.updatePosition()
+            highway.checkChangeLaneLeft()
+            highway.checkChangeLaneRight()
+            highway.enterAtStart(0.1)
+            highway.exitAtEnd()
+            highway.entranceEvent(0.3, 0.4)
+            
+            # send data to UI
+            GUI.display(highway)
+    
+    workerThread = threading.Thread(target=run)
+    workerThread.setDaemon(True)
+    workerThread.start()
+    
+    GUI.mainloop()
 
 
 if __name__ == '__main__':
-	main()
+    main()
