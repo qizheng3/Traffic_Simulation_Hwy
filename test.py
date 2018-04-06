3ee5161288d4ca7a77cb955934dbf0b9e7d6295a
qizheng3 <qizheng03@gmail.com>
2018-04-06 06:09:04 -0400

    done with map digitization

---
+++ test.py
@@ -0,0 +1,18 @@
+from config import *
+
+def ppt():
+    global MAPS
+    for pr in MAPS:
+        plt.scatter ([x for (x, _) in pr], [y for (_, y) in pr])
+    plt.show ()
+
+    pr = MAPS[0]
+    plt.plot([pr[0][0], pr[JOIN_ID[0]][0]], [pr[0][1], pr[JOIN_ID[0]][1]])
+    plt.plot([pr[JOIN_ID[0]+1][0], pr[JOIN_ID[1]][0]], [pr[JOIN_ID[0]+1][1], pr[JOIN_ID[1]][1]])
+    plt.plot([pr[JOIN_ID[1] + 1][0], pr[JOIN_ID[2]][0]], [pr[JOIN_ID[1] + 1][1], pr[JOIN_ID[2]][1]])
+    plt.plot([pr[JOIN_ID[2] + 1][0], pr[-1][0]], [pr[JOIN_ID[2] + 1][1], pr[-1][1]])
+    plt.show()
+    # print pr[0]
+    
+    
+ppt()
